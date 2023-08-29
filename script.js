f53e3e1164e0b4c3038f309c3cf334d4b6b8f594
falsumit <bclarke516@outlook.com>
2023-08-29 19:30:51 -0400

    fixed text overflow in results

--- script.js
+++ script.js
@@ -17,10 +17,64 @@ const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
 
 let input = [0];
+let answer;
 
 one.addEventListener('click', () => {
     input.push(1);
+    result.innerHTML = 1;
 });
+
+two.addEventListener('click', () => {
+    input.push(2);
+    result.innerHTML = 2;
+});
+
+three.addEventListener('click', () => {
+    input.push(3);
+    result.innerHTML = 3;
+});
+
+four.addEventListener('click', () => {
+    input.push(4);
+    result.innerHTML = 4;
+});
+
+five.addEventListener('click', () => {
+    input.push(5);
+    result.innerHTML = 5;
+});
+
+six.addEventListener('click', () => {
+    input.push(6);
+    result.innerHTML = 6;
+});
+
+seven.addEventListener('click', () => {
+    input.push(7);
+    result.innerHTML = 7;
+});
+
+eight.addEventListener('click', () => {
+    input.push(8);
+    result.innerHTML = 8;
+});
+
+nine.addEventListener('click', () => {
+    input.push(9);
+    result.innerHTML = 9;
+});
+
+zero.addEventListener('click', () => {
+    input.push(0);
+    result.innerHTML = 0;
+});
+
+
+
+
+
+
+
 
 equals.addEventListener('click', () => {
     result.innerHTML = input;
