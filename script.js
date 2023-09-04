5ce21a1b3bc5ac1f059dadfa0863e0415bd70075
falsumit <brooks516@outlook.com>
2023-09-04 18:00:58 -0400

    all calc functions seem to be working properly. I will continue to test for issues

--- script.js
+++ script.js
@@ -16,149 +16,192 @@ const add = document.getElementById('add');
 const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
 
-// let inputArray = [0];
-let a = '';
-let b = '';
-let answer = '';
+let a, b, answer;
+let divi = false;
+let mult = false;
+let subt = false;
+let addi = false;
+let i = 0;
 
 one.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(1);
 });
 
 two.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(2);
 });
 
 three.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(3);
 });
 
 four.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(4);
 });
 
 five.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(5);
 });
 
 six.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(6);
 });
 
 seven.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(7);
 });
 
 eight.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(8);
 });
 
 nine.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(9);
 });
 
 zero.addEventListener('click', () => {
-    if (result.innerHTML == 0 ||
-        result.innerHTML == '+' ||
-        result.innerHTML == '-' ||
-        result.innerHTML == '*' ||
-        result.innerHTML == '/') {
-        result.innerHTML = '';
+    if (result.textContent == 0 ||
+        result.textContent == '+' ||
+        result.textContent == '-' ||
+        result.textContent == '*' ||
+        result.textContent == '%' ||
+        i > 0) {
+            result.textContent = '';
+            i = 0;
     }
     result.append(0);
 });
 
-add.addEventListener('click', () => {
-    a = result.innerHTML;
-    result.innerHTML = '+';
+divide.addEventListener('click', () => {
+    a = result.textContent;
+    result.textContent = '%';
+    divi = true;
 });
 
+multiply.addEventListener('click', () => {
+    a = result.textContent;
+    result.textContent = '*';
+    mult = true;
+});
 
+subtract.addEventListener('click', () => {
+    a = result.textContent;
+    result.textContent = '-';
+    subt = true;
+});
 
+add.addEventListener('click', () => {
+    a = result.textContent;
+    result.textContent = '+';
+    addi = true;
+});
 
 equals.addEventListener('click', () => {
-    b = result.innerHTML;
-    answer = +a + +b;
-    result.innerHTML = answer;
+    b = result.textContent;
+    if (divi == true) {
+        answer = +a / +b;
+        divi = false;
+    } else if (mult == true) {
+        answer = +a * +b;
+        mult = false;
+    } else if (subt == true) {
+        answer = +a - +b;
+        subt = false;
+    } else if (addi == true) {
+        answer = +a + +b;
+        addi = false;
+    }
+    result.textContent = answer;
+    i++
     a = answer;
     b = '';
     answer = '';
-    console.log(a);
-    console.log(b);
-    console.log(answer);
 });
 
-// equals.addEventListener('click', () => {
-//     input.splice(0, 1);
-//     let newResult = input.toString();
-//     result.innerHTML = newResult;
-// });
-
 clear.addEventListener('click', () => {
-    result.innerHTML = '0';
+    result.textContent = '0';
+    i = 0;
     a = '';
     b = '';
     answer = '';
